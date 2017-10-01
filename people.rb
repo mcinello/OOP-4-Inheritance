9a58d44b472a87837ad00dbd111fa72e257ae41f
Michelle Cinello <m.cinello@gmail.com>
2017-10-01 15:52:10 -0400

    Instructor able to greet herself

---
+++ people.rb
@@ -0,0 +1,41 @@
+class Person
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def greeting
+    puts "Hi, my name is #{name}"
+  end
+
+end
+
+
+class Student < Person
+
+  def learn
+    puts "I get it!"
+  end
+
+end
+
+
+
+
+
+class Instructor < Person
+
+  def teach
+    puts "Everything in Ruby is an object"
+  end
+
+end
+
+
+instructor = Person.new("Nadia")
+
+instructor.greeting

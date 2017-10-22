04d607c2d72944f7f21ed5f870c89febcdb00afc
Michelle Cinello <m.cinello@gmail.com>
2017-10-22 19:14:09 -0400

    able to output farm's amount of harvested food

--- farm-simulator.rb
+++ farm-simulator.rb
@@ -3,17 +3,18 @@ class Farm
 # farm wants to keep track of fields on the farm (corn and wheat)
   @@fields = []
 
-  def self.add(field)
+# add field to farm
+  def self.field(field)
     @@fields << field
   end
 
   # total amount of food farm has produced
-  def self.total_amount_of_food
+  def self.harvest
     sum = 0
     @@fields.each do |food|
-      sum += food
+      sum += food.food_per_hectare
     end
-    return sum
+    puts "The farm has #{sum} harvested food so far."
   end
 
   def self.fields
@@ -38,20 +39,32 @@ class Field
     @food_per_hectare
   end
 
-  def field_type(type)
-    @field_type = type
-    @field_type
-  end
-
-  def food_per_hectare(num)
-    @food_per_hectare = num
-    @food_per_hectare
-  end
+  # def field_type(type)
+  #   @field_type = type
+  #   @field_type
+  # end
+  #
+  # def food_per_hectare(num)
+  #   @food_per_hectare = num
+  #   @food_per_hectare
+  # end
 
 end
 
-# adding field to farm
-Farm.add(Field.new("corn", 20))
-Farm.add(Field.new("wheat", 30))
+class Corn < Field
 
-puts Farm.fields.inspect
+end
+
+class Wheat < Field
+
+end
+
+
+
+
+
+# adding field to farm
+Farm.field(Field.new("corn", 0))
+Farm.field(Field.new("wheat", 0))
+
+puts Farm.harvest
